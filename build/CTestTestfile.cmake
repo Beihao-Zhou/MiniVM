d06fb35a069f5644671cfe01480795c676b680c4
Beihao Zhou <beihaozhou@gmail.com>
2024-06-23 17:21:52 -0700

    clean up build artifects

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/beihaozhou/Projects/miniVM
-# Build directory: /Users/beihaozhou/Projects/miniVM/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/beihaozhou/Projects/miniVM/build/vm_test[1]_include.cmake")
